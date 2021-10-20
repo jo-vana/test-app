08e37e86fd01fdfde0568273b9b195a567c88b3b
jo-vana <jovananukic85@gmail.com>
2021-10-20 16:31:22 +0200

    Creating reactive form for EditUser and returning the values

--- src/app/users/edit-user/edit-user.component.ts
+++ src/app/users/edit-user/edit-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../user.service';
 
@@ -15,6 +16,8 @@ export class EditUserComponent implements OnInit {
   userBirth='';
   changesSaved = false;
 
+  profileForm: FormGroup;
+
   constructor(private usersService: UserService,
               private route: ActivatedRoute,
               private router: Router) { }
@@ -27,6 +30,21 @@ export class EditUserComponent implements OnInit {
     this.userFirst = this.user.firstName;
     this.userLast = this.user.lastName;
     this.userBirth = this.user.dateOfBirth;
+
+    this.profileForm = new FormGroup({
+      'firstName': new FormControl(null, [Validators.required]),
+      'lastName': new FormControl(null, [Validators.required]),
+      'dateOfBirth': new FormControl(null, [Validators.required])
+    });
+
+    this.profileForm.setValue({
+        // firstName: 'Tim',
+        // lastName: 'Roth',
+        // dateOfBirth: '6/15/1970'
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        dateOfBirth: this.user.dateOfBirth
+    })
   }
 
   onUpdateUser() {
@@ -34,5 +52,9 @@ export class EditUserComponent implements OnInit {
     this.usersService.updateUser(this.user.id, {firstName: this.userFirst, lastName: this.userLast, dateOfBirth: this.userBirth});
     this.changesSaved = true;
     this.router.navigate(['../../'], {relativeTo: this.route});
+  }
+
+  onSubmit() {
+    console.log(this.profileForm);
   }
 }
